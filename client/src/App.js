ce88f590f1d434ff032aeba4cce1c9defafe234e
vloparev <vloparev@bekitzur.com>
2021-07-26 22:33:14 +0300

    AWS deploy

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <Link to="/">Home</Link>
-                    <Link to="/otherpage">Other Page</Link>
+                    <Link to="/otherpage">Other Page Link</Link>
                 </header>
                 <div>
                     <Route exact path="/" component={Fib}/>
